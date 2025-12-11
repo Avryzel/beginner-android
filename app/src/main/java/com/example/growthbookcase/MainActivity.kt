733466fababde1caf3dfbd5be245ba28c0a9e459
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-12-11 22:00:51 +0700

    fix: strings.xml to resolve XML parsing error and feat: implement MainActivity list data logic

--- app/src/main/java/com/example/growthbookcase/MainActivity.kt
+++ app/src/main/java/com/example/growthbookcase/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.growthbookcase
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvBooks: RecyclerView
+    private val list = ArrayList<Book>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        rvBooks = findViewById(R.id.rv_books)
+        rvBooks.setHasFixedSize(true)
+
+        list.addAll(getListBooks())
+        showRecycleList()
+    }
+
+    private fun getListBooks(): ArrayList<Book> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description_detail)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listBook = ArrayList<Book>()
+
+        for (i in dataName.indices) {
+            val book = Book(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listBook.add(book)
         }
+        dataPhoto.recycle()
+        return listBook
+    }
+
+    private fun showRecycleList() {
+        rvBooks.layoutManager = LinearLayoutManager(this)
+        val listBookAdapter = ListBookAdapter(list)
+        rvBooks.adapter = listBookAdapter
     }
 }

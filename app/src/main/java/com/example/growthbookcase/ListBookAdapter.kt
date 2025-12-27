4de600354e1b588ed009ff5863050e11a3d9f8b6
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-12-27 20:10:38 +0700

    feat: implement item click callback in ListBook Adapter

--- app/src/main/java/com/example/growthbookcase/ListBookAdapter.kt
+++ app/src/main/java/com/example/growthbookcase/ListBookAdapter.kt
@@ -10,6 +10,12 @@ import androidx.recyclerview.widget.RecyclerView
 class ListBookAdapter(private val listBook: ArrayList<Book>) :
     RecyclerView.Adapter<ListBookAdapter.ListViewHolder>() {
 
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -27,6 +33,8 @@ class ListBookAdapter(private val listBook: ArrayList<Book>) :
         holder.imgPhoto.setImageResource(image)
         holder.tvName.text = name
         holder.tvDescription.text = description
+
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listBook[holder.adapterPosition]) }
     }
 
     override fun getItemCount(): Int = listBook.size
@@ -35,5 +43,9 @@ class ListBookAdapter(private val listBook: ArrayList<Book>) :
         val imgPhoto: ImageView = itemView.findViewById(R.id.img_book_photo)
         val tvName: TextView = itemView.findViewById(R.id.tv_book_name)
         val tvDescription: TextView = itemView.findViewById(R.id.tv_book_description)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Book)
     }
 }

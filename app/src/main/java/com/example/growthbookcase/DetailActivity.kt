075e847f03c76c250884ed6b1246251c8e0d118f
Avryzel <128247566+Avryzel@users.noreply.github.com>
2026-01-08 21:33:36 +0700

    feat: implement share button logic for detail screen

--- app/src/main/java/com/example/growthbookcase/DetailActivity.kt
+++ app/src/main/java/com/example/growthbookcase/DetailActivity.kt
@@ -1,36 +1,58 @@
 package com.example.growthbookcase
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
-
     companion object {
         const val KEY_BOOK = "key_book"
     }
+
+    private var book: Book? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val dataBook = if (Build.VERSION.SDK_INT >= 33) {
+        book = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra(KEY_BOOK, Book::class.java)
         } else {
             @Suppress("DEPRECATION")
             intent.getParcelableExtra(KEY_BOOK)
         }
 
-        if (dataBook != null) {
+        if (book != null) {
             val tvImage: ImageView = findViewById(R.id.detail_image)
             val tvDetailTitle: TextView = findViewById(R.id.detail_title)
             val tvDetailDescription: TextView = findViewById(R.id.detail_description)
 
-            tvImage.setImageResource(dataBook.image)
-            tvDetailTitle.text = dataBook.name
-            tvDetailDescription.text = dataBook.description
+            book?.image?.let { tvImage.setImageResource(it) }
+            tvDetailTitle.text = book?.name
+            tvDetailDescription.text = book?.description
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.share_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_share) {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+
+            val shareText = "Book: ${book?.name}\n\nDescription: ${book?.description}"
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+            startActivity(Intent.createChooser(shareIntent, "Share with"))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
